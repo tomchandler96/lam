c44c8f18bb0e69657b97492ce82ebffddd011030
tomchandler96 <ndlambn96@gmail.com>
2026-03-11 10:38:37 +0700

    Update Lan_Share.bat

--- Lan_Share.bat
+++ Lan_Share.bat
@@ -2,34 +2,44 @@
 title LAN SHARE TOOL - IT SUPPORT
 color 0A
 
+:: Kiem tra quyen admin
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+echo Dang yeu cau quyen Administrator...
+powershell -Command "Start-Process '%~f0' -Verb runAs"
+exit
+)
+
 :menu
 cls
-echo ===============================
-echo        LAN SHARE TOOL
-echo ===============================
+echo =====================================
+echo            LAN SHARE TOOL
+echo =====================================
 echo 1. Cai dat MAY CHIA SE (May con)
 echo 2. Ket noi MAY CHU (Map o mang)
 echo 3. Reset mang
 echo 4. Mo thu muc chia se
 echo 5. Xem IP may
 echo 0. Thoat
-echo ===============================
+echo =====================================
 
 set /p choice=Chon chuc nang: 
 
-if %choice%==1 goto share
-if %choice%==2 goto connect
-if %choice%==3 goto resetnet
-if %choice%==4 goto open
-if %choice%==5 goto ip
-if %choice%==0 exit
+if "%choice%"=="1" goto share
+if "%choice%"=="2" goto connect
+if "%choice%"=="3" goto resetnet
+if "%choice%"=="4" goto open
+if "%choice%"=="5" goto ip
+if "%choice%"=="0" exit
 
 goto menu
 
 
 :share
 cls
-echo Dang cai dat may chia se...
+echo =====================================
+echo Dang cai dat may chia se LAN...
+echo =====================================
 
 :: Bat Network Discovery
 netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
@@ -41,16 +51,22 @@ netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=
 sc config lanmanserver start= auto
 net start lanmanserver
 
-:: Tat password sharing
+:: Fix loi hoi mat khau LAN Win10/11
 reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 1 /f
+
+:: Cho phep guest SMB
+reg add HKLM\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters /v AllowInsecureGuestAuth /t REG_DWORD /d 1 /f
+
+:: Bat tai khoan Guest
+net user guest /active:yes
 
 :: Share o D
 net share DATA=D:\ /grant:everyone,full
 
-echo ===============================
+echo =====================================
 echo Da chia se o D voi ten DATA
 echo Truy cap bang: \\IP-MAY-CON\DATA
-echo ===============================
+echo =====================================
 pause
 goto menu
 
@@ -61,9 +77,9 @@ set /p ip=Nhap IP may chia se:
 
 net use Z: \\%ip%\DATA /persistent:yes
 
-echo ===============================
-echo Da map o mang Z:
-echo ===============================
+echo =====================================
+echo Da map o Z:
+echo =====================================
 pause
 goto menu
 
@@ -78,7 +94,9 @@ ipconfig /renew
 netsh winsock reset
 netsh int ip reset
 
-echo Reset xong. Nen restart may.
+echo =====================================
+echo Reset xong. Nen restart may
+echo =====================================
 pause
 goto menu
 
@@ -91,6 +109,7 @@ goto menu
 
 
 :ip
+cls
 ipconfig
 pause
 goto menu
